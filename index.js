bf73c7cd6cbef235c0d6a8f1aa3cb9b3824fe08e
opariche <oscar.pariche@gmail.com>
2023-10-29 11:41:07 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+for (let i = 1; i < 101; i++){
+   if (i % 15 === 0) {
+    console.log('FIZZBUZZ');
+   }
+   else if (i % 3 === 0) {
+    console.log('FIZZ');
+   }
+else if (i % 5 === 0){
+    console.log('BUZZ');
+}
+else {
+    console.log(i);
+}
+};
